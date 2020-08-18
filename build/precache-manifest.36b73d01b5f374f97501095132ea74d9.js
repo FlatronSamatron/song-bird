de60752235f238d494a3825e3e6720eb1800fa3b
flatronsamatron <flatronsamatron@gmail.com>
2020-08-18 16:40:05 +0300

    add buid

---
+++ build/precache-manifest.36b73d01b5f374f97501095132ea74d9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f78f4376785ccf14e1769588baf123",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8060198baecd590103ba",
+    "url": "/static/css/2.9131a3d6.chunk.css"
+  },
+  {
+    "revision": "fbc828112cdaf6372806",
+    "url": "/static/css/main.c7438cc6.chunk.css"
+  },
+  {
+    "revision": "8060198baecd590103ba",
+    "url": "/static/js/2.ebcef8db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ebcef8db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc828112cdaf6372806",
+    "url": "/static/js/main.41e0dc77.chunk.js"
+  },
+  {
+    "revision": "dc9173b7e8edec60da3a",
+    "url": "/static/js/runtime-main.3ffde9c1.js"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "/static/media/bird.06a46938.jpg"
+  },
+  {
+    "revision": "165166d5c9e027367cb75b2ac89dcfc0",
+    "url": "/static/media/error.165166d5.mp3"
+  },
+  {
+    "revision": "4f82cd73f3446ef1af7fa6d3a302ef3a",
+    "url": "/static/media/logo.4f82cd73.svg"
+  },
+  {
+    "revision": "a1e9e8b671592af8b3a503b23bbba6eb",
+    "url": "/static/media/win.a1e9e8b6.mp3"
+  }
+]);
